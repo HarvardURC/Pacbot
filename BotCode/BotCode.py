be356ee31af12c558932281b862ef3b724325b92
vagrant <vagrant@CS61-Appliance>
2017-04-16 16:30:23 +0000

    Miles's pseudocode

--- BotCode/BotCode.py
+++ BotCode/BotCode.py
@@ -5,6 +5,9 @@ from time import sleep
 
 #Variables 
 w, h = 28, 31;
-Grid = [[0 for x in range(w)] for y in range(h)] 
-Grid = 
+Grid = [['w' for x in range(w)] for y in range(h)] 
 #Function for Receiving the signal and filling out the table 
+def receive_move(): 
+
+
+def main():
