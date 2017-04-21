537c089f8a5bac177d7b15b44299b600bf229353
vagrant <vagrant@CS61-Appliance>
2017-04-21 02:43:08 +0000

    grid

--- BotCode/BotCode.c
+++ BotCode/BotCode.c
@@ -1,7 +1,6 @@
 // Initialize libraries
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdint.h>
 #include <string.h>
 #include <inttypes.h>
 #include <math.h>
@@ -9,69 +8,103 @@
 #include <assert.h>
 #include <time.h>
 #include <sys/time.h>
-#include <Motors.h>
+#include <stdio.h>
+//#include <wiringPi.h>
+//#include <Direction.c> 
 
 
 // Initialize connection with camera
-
+//Must have two modes 
 
 // Initialize Variables
 
 	// need a variable to keep track of cardinal direction state
 		// card_state = 1, 2, 3, 4. 1 being north, 2 being east, 3 being south, 4 being west
+#define NORTH 	= 1;
+#define EAST 	= 2;
+#define SOUTH 	= 3;
+#define WEST 	= 4;
+//Pacman initial Position
+#define ROW = 22;
+#define COL = 13; 
+typedef struct cell_cordinates{
+	int y; 
+	int x;
+}cell_cordinates;
+//food opt = pellet =1, big pellet =  2 ; empty= 3; fruit = 4  
+typedef struct game_state
+{
+	cell_cordinates cur_pos; 
+	int dir; 
+}game_state;
 
-// Initialize Grid
-
-typedef struct cell_status {
+typedef struct free_cell {
+    cell_cordinates cordinates; 
     int pacman_pos;
     int ghost_pos;
-    char food_opt;  
-} cell_status;
+    char food_opt; 
+ 	cell_cordinates adj_cell[4]; 
+    int directions[4]; 
+}free_cell;
 
-int main(int argc, char* argv[]) {
-	char* grid =  malloc(sizeof(cell_status)*31*28);
+// Declare Grid as global 
+free_cell *grid; 
+void createGrid(){
+	grid = (free_cell*) malloc(868*sizeof(free_cell)); 
+    FILE *fp;
+    int a; 
+    int b; 
+    char c;  
+    fp = fopen("freespaces.txt", "r"); // I open the file to read it("r")
+    while(fscanf (fp, "%d,%d, %s ", &a, &b, &c)==3) {  
+    	free_cell new_cell; 
+    	new_cell.cordinates.x = a; 
+    	new_cell.cordinates.y = b;
+    	new_cell.pacman_pos = 0; 
+    	new_cell.ghost_pos =0; 
+    	new_cell.food_opt = c; 
+    	new_cell.adj_cell[0].x = 0; 
+    	new_cell.adj_cell[1].x = 0; 
+    	new_cell.adj_cell[2].x = 0; 
+    	new_cell.adj_cell[3].x = 0; 
+    	new_cell.adj_cell[0].y = 0; 
+    	new_cell.adj_cell[1].y = 0; 
+    	new_cell.adj_cell[2].y = 0; 
+    	new_cell.adj_cell[3].y = 0;   
+    	new_cell.directions[0]= 0;
+    	new_cell.directions[1]= 0; 
+    	grid[b + 28*a] = new_cell;     
+    }
+   
 
+}
+//N=1, E=2 S=3 W=4
+game_state * get_successor(game_state current_node){
+	static game_state successors[4];
+	int x = current_node.cur_pos.x; 
+	int y = current_node.cur_pos.y;
+	int d = current_node.dir; 
+	for(int i = 0; i<4; i++)
+		successors[i].cur_pos = grid[28*x + y].adj_cell[i]; 
+	return successors; 
+}
+
+int main(int argc, char** argv) {
+	createGrid();
+	for(int i=0; i<=28; i++){
+		for(int j=0; j<31; j++){
+			// if(grid[i + 28*j].cordinates.x == 0 && grid[j*28 + i].cordinates.y == 0)
+			// 	printf("%s", "w");
+			// else
+				//printf("%s","_");
+				printf("(%d,%d)",grid[j*28 + i].cordinates.x, grid[j*28 + i].cordinates.y);
+		}
+		printf("\n");
+	}
 
 	return 0; 
 }
-
-// Update Grid
-
-	// starting grid
-
-/*
-
-walls
-
-(0,0) (1,0) (2,0) (3,0) (4,0) (5,0) (6,0) (7,0) (8,0) (9,0) 
-(10,0) (11,0) (12,0) (13,0) (14,0) (15,0) (16,0) (17,0) (18,0) (19,0) 
-(20,0) (21,0) (22,0) (23,0) (24,0) (25,0) (26,0) (27,0) 
-
-(0,1) (27,1)
-
-(0,2) (2,2) (3,2) (4,2) (5,2) (6,2) (7,2) (8,2) (9,2) (10,2) (11,2) (13,2) 
-(14,2) (16,2) (17,2) (18,2) (19,2) (20,2) (21,2) (22,2) (23,2) (24,2) (25,2) (27,2) 
-
-(0,3) (2,3) (3,3) (4,3) (5,3) (6,3) (7,3) (8,3) (9,3) (10,3) (11,3) (13,3) 
-(14,3) (16,3) (17,3) (18,3) (19,3) (20,3) (21,3) (22,3) (23,3) (24,3) (25,3) (27,3)
-
-(0,4) (7,4) (8,4) (13,4) (14,4) (19,4) (20,4) (27,4)
-
-(0,5) (1,5) (2,5) (4,5) (5,5) (7,5) (8,5) (10,5) (11,5) (12,5) (13,5) (14,5) (15,5)
-(16,5) (17,5) (19,5) (20,5) (22,5) (23,5) (25,5) (26,5) (27,5) 
-
-(0,6) (1,6) (2,6) (4,6) (5,6) (7,6) (8,6) (10,6) (11,6) (12,6) (13,6) (14,6) (15,6)
-(16,6) (17,6) (19,6) (20,6) (22,6) (23,6) (25,6) (26,6) (27,6) 
-
-
-
-(0,30) (1,30) (2,30) (3,30) (4,30) (5,30) (6,30) (7,30) (8,30) (9,30) 
-(10,30) (11,30) (12,30) (13,30) (14,30) (15,30) (16,30) (17,30) (18,30) (19,30) 
-(20,30) (21,30) (22,30) (23,30) (24,30) (25,30) (26,30) (27,30) 
-
-*/ 
-
-	// incoming camera updates
+ // incoming camera updates
 
 // Output Grid as variables (a tuple maybe?) for determining strategies
 
