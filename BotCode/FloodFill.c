92eac5681f967b2672ac68cc0e62d0e8a0f37e00
timothy.tamm <tamm@college.harvard.edu>
2017-04-22 06:46:52 -0400

    fix floodfill

--- BotCode/FloodFill.c
+++ BotCode/FloodFill.c
@@ -8,13 +8,6 @@
 #include "AStar.h"
 #include "FloodFill.h"
 
-
-int direc[4];
-direc[0] = 1;
-direc[1] = 2;
-direc[2] = 3;
-direc[3] = 1;
-
 //Returns the head node and moves head pointer to next node
 int pop_flood_node(flood_node **head, flood_node **tail, flood_node **ret){
 	if(*head == NULL) {
@@ -29,13 +22,15 @@ int pop_flood_node(flood_node **head, flood_node **tail, flood_node **ret){
 }
 
 //returns up to four possible next moves
-void add_legal_successors(int new_depth, free_cell pos, uint8_t *visited, flood_node **head, flood_node **tail, int direc){
+void add_legal_successors(int new_depth, free_cell pos, uint8_t *visited,
+        flood_node **head, flood_node **tail, int g_dir) {
 	free_cell adjacent_cell;
 	for(int i=0; i<4; i++){
 		adjacent_cell = grid[28*pos.adj_cell[i].cp_x +pos.adj_cell[i].cp_y];  
             printf("%s\n", "check" ); 
-		if( (adjacent_cell.food_opt != 'w') &&
-                (*(visited+28*pos.adj_cell[i].cp_x + pos.adj_cell[i].cp_y) != 1) && direc != (i + 3) % 4) {
+		if( (adjacent_cell.food_opt != 'w') && 
+                !(new_depth == 1 && ((i+1 - g_dir) % 4) == 2) &&
+                (*(visited+28*pos.adj_cell[i].cp_x + pos.adj_cell[i].cp_y) != 1)) {
             *(visited+28*pos.adj_cell[i].cp_x + pos.adj_cell[i].cp_y) = 1;
 			flood_node *new_flood_node = malloc(sizeof(flood_node));
             if (new_flood_node == NULL) {
@@ -45,8 +40,6 @@ void add_legal_successors(int new_depth, free_cell pos, uint8_t *visited, flood_
 			new_flood_node->current_cell = adjacent_cell; 
 			new_flood_node->next_flood_node = NULL; 
             new_flood_node->depth = new_depth;
-            new_flood_node->direction = i+1; 
-            direc[i] = i + 1;
             if (*head == NULL) {
                 *head = new_flood_node;
                 *tail = *head;
@@ -68,7 +61,7 @@ void clear_ghost_danger(){
 }
 
 //on new ghost position update, change the main grid danger variable to match distance from ghosts
-void ghost_flood(/*ghost_dir* direcs*/){
+void ghost_flood(ghost_dir *direcs) {
 	cell_pos ghosts [4]; 
     /*
 	ghosts[0] = getInky();
@@ -84,6 +77,11 @@ void ghost_flood(/*ghost_dir* direcs*/){
 	ghosts[2].cp_y = 21;  //getPinky();
 	ghosts[3].cp_x = 1;
 	ghosts[3].cp_y = 1; //getClyde();
+    int directions[4];
+    directions[0] = direcs->ink_dir;
+    directions[1] = direcs->blink_dir;
+    directions[2] = direcs->pink_dir;
+    directions[3] = direcs->clyde_dir;
     uint8_t visited[868];
 	clear_ghost_danger();
 	for(int i=0; i<4; i++) {
@@ -93,23 +91,11 @@ void ghost_flood(/*ghost_dir* direcs*/){
             printf("EOM. can't handle\n");
             exit(1);
         }
-
-        /*int direc;
-        if (i == 0)
-            direc = direcs->ink_dir;
-        else if (i == 1)
-            direc = direcs->blink_dir;
-        else if (i == 2)
-            direc = direcs->pink_dir;
-        else
-            direc = direcs->clyde_dir;
-        */
 		flood_node *tail = head_fringe;
         free_cell temp = grid[28*ghosts[i].cp_x + ghosts[i].cp_y];
 		head_fringe->current_cell =  temp;
         *(visited+28*ghosts[i].cp_x + ghosts[i].cp_y) = 1;
 		head_fringe->depth = 0;
-        head_fringe->direction = direc[i]; 
         head_fringe->next_flood_node = NULL;
 		while (1) {
             flood_node *cur_node;
@@ -121,9 +107,12 @@ void ghost_flood(/*ghost_dir* direcs*/){
             if (empty) {
                 printf("should not reach here!\n");
             }
-			add_legal_successors(cur_node->depth+1, cur_node->current_cell, &visited[0], &head_fringe, &tail, direc[i]);
-            if (grid[28*cur_node->current_cell.coordinates.cp_x + cur_node->current_cell.coordinates.cp_y].ghost_danger > cur_node->depth) {
-			    grid[28*cur_node->current_cell.coordinates.cp_x + cur_node->current_cell.coordinates.cp_y].ghost_danger = cur_node->depth; 
+			add_legal_successors(cur_node->depth+1, cur_node->current_cell,
+                    &visited[0], &head_fringe, &tail, directions[i]);
+            if (grid[28*cur_node->current_cell.coordinates.cp_x +
+                    cur_node->current_cell.coordinates.cp_y].ghost_danger > cur_node->depth) {
+			    grid[28*cur_node->current_cell.coordinates.cp_x +
+                    cur_node->current_cell.coordinates.cp_y].ghost_danger = cur_node->depth; 
             }
             free(cur_node);
 		}
--- BotCode/FloodFill.h
+++ BotCode/FloodFill.h
@@ -15,18 +15,17 @@ typedef struct flood_node{
 	struct flood_node *next_flood_node;
 	free_cell current_cell;  
     int depth;
-    int direction; 
 } flood_node; 
 
 
 //Returns the head node and moves head pointer to next node
 int pop_flood_node(flood_node **head, flood_node **tail, flood_node **ret);
 
-//returns up to four possible next moves
-void add_legal_successors(int new_depth, free_cell pos, uint8_t *visited, flood_node **head, flood_node **tail, int direc);
+void add_legal_successors(int new_depth, free_cell pos, uint8_t *visited,
+        flood_node **head, flood_node **tail, int g_dir);
 
 void clear_ghost_danger();
 //on new ghost position update, change the main grid danger variable to match distance from ghosts
-void ghost_flood(/*ghost_dir* direcs*/);  
+void ghost_flood(ghost_dir *direcs);
 
 #endif /* _FLOOD_FILL_H_ */
