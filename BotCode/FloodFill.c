4cb1b2a3ed92c9b836fd42eefd228bdfe49aadfe
timothy.tamm <tamm@college.harvard.edu>
2017-04-22 01:55:37 -0400

    getting there

--- BotCode/FloodFill.c
+++ BotCode/FloodFill.c
@@ -1,96 +1,76 @@
-#include "AStar.h"
 #include <math.h>
 #include <stdlib.h>
-#include "FloodFill.h"
 #include <limits.h>
-#include "grid.h"
 #include <stdio.h>
+#include <inttypes.h>
+#include <string.h>
+#include "grid.h"
 #include "state.h"
-//Add a node n to the end of current_node
-void push_flood_node(flood_node* n, flood_node * current_node){ 
-	printf("%d\n", n->current_cell.coordinates.cp_x);
-	printf("%d\n", n->current_cell.coordinates.cp_y);
-	if(n==NULL)
-		return; 
-	while(current_node->next_flood_node != NULL){
-		current_node->next_flood_node = (flood_node*) malloc(sizeof(flood_node));
-		current_node= current_node->next_flood_node; 
-	}
-	printf("3\n");
-	current_node->next_flood_node = n; 
-	printf("%d\n", current_node->next_flood_node->current_cell.coordinates.cp_x);
-	printf("%d\n", current_node->next_flood_node->current_cell.coordinates.cp_y);
-	printf("4\n");
-	current_node = n; 
-	current_node->next_flood_node = NULL;
-	printf("%d\n", current_node->current_cell.coordinates.cp_x);
-	printf("%d\n", current_node->current_cell.coordinates.cp_y); 
-	printf("5");
-} 
+#include "AStar.h"
+#include "FloodFill.h"
+
 //Returns the head node and moves head pointer to next node
-free_cell pop_flood_node(flood_node* head){
-	if(head == NULL)
-		return head->current_cell; 
-	free_cell cell = head->current_cell;
-
-	if(head->next_flood_node == NULL)
-		head =  NULL; 
-	else
-	head = head->next_flood_node;
-
-	return cell;
+int pop_flood_node(flood_node **head, flood_node **tail, flood_node **ret){
+	if(*head == NULL) {
+        return 1; 
+    }
+	*ret = *head;
+    *head = (*head)->next_flood_node;
+    if (*head == NULL) {
+        *tail = NULL;
+    }
+	return 0;
 }
-//returns 0 if our list does not contain pos 1 if it does 
-int  closed_contains(free_cell cell, flood_node * n){
-	while(n->next_flood_node != NULL){
-		if(cell.coordinates.cp_x == n->current_cell.coordinates.cp_x && cell.coordinates.cp_y == n->current_cell.coordinates.cp_y)
-			return 1; 
-		else 
-			n = n->next_flood_node; 
-	}
-	return 0; 
-}
+
 //returns up to four possible next moves
-flood_node * get_legal_successors(free_cell pos, int depth){
-	flood_node * nodes_to_return = NULL; 	
+void add_legal_successors(int new_depth, free_cell pos, uint8_t *visited, flood_node **head, flood_node **tail){
 	free_cell adjacent_cell; 
 	for(int i=0; i<4; i++){
 		adjacent_cell = grid[28*pos.adj_cell[i].cp_y +pos.adj_cell[i].cp_x];  
-		if(adjacent_cell.ghost_danger > depth){
-			adjacent_cell.ghost_danger = depth; 
-		}
-		if((adjacent_cell.food_opt != 'w') && (adjacent_cell.ghost_danger > pos.ghost_danger + 1)){
-			flood_node * new_flood_node; 
-			new_flood_node-> current_cell = adjacent_cell; 
-			new_flood_node -> next_flood_node = NULL; 
-			push_flood_node(new_flood_node, nodes_to_return); 
+		if( (adjacent_cell.food_opt != 'w') &&
+                (*(visited+28*pos.adj_cell[i].cp_y +pos.adj_cell[i].cp_x) != 1)) {
+            *(visited+28*pos.adj_cell[i].cp_y +pos.adj_cell[i].cp_x) = 1;
+			flood_node *new_flood_node = malloc(sizeof(flood_node)); 
+            if (new_flood_node == NULL) {
+                printf("EOM. can't handle\n");
+                exit(1);
+            }
+			new_flood_node->current_cell = adjacent_cell; 
+			new_flood_node->next_flood_node = NULL; 
+            new_flood_node->depth = new_depth;
+            printf("adding node at depth %d\n", new_depth);
+            printf("head is %llu\n", (unsigned long long) *head);
+            if (*head == NULL) {
+                *head = new_flood_node;
+                *tail = *head;
+            } else if (*head == *tail) {
+                (*head)->next_flood_node = new_flood_node;
+                *tail = new_flood_node;
+            } else {
+                (*tail)->next_flood_node = new_flood_node;
+                *tail = new_flood_node;
+            }
+            printf("success\n");
 		}
 	}
-	return nodes_to_return; 
-} 
+}
+
 void clear_ghost_danger(){
 	for(int i=0; i<868; i++){
 		grid[i].ghost_danger= INT_MAX; 
 	}
 }
-void freer_function(flood_node* head){
-	flood_node* new_head; 
-	while(head->next_flood_node != NULL){
-		new_head = head->next_flood_node; 
-		free(head); 
-	}
-	free(new_head);
-}
-// void print_linked_list(flood_node * head){
-// 	flood_node * head_t = head; 
-// 	while(head != NULL)
-// 		printf("%d,%d\n", head->current_cell.coordinates.cp_x,head->current_cell.coordinates.cp_x);
 
-// }
 //on new ghost position update, change the main grid danger variable to match distance from ghosts
 void ghost_flood(){
 	cell_pos ghosts [4]; 
-	ghosts[0].cp_x = 9;//getInky();
+    /*
+	ghosts[0] = getInky();
+	ghosts[1] = getBlinky();
+	ghosts[2] = getPinky();
+	ghosts[3] = getClyde();
+    */
+    ghosts[0].cp_x = 9;//getInky();
 	ghosts[0].cp_y = 14; 
 	ghosts[1].cp_x = 15;//getBlinky();
 	ghosts[1].cp_x = 8;
@@ -98,38 +78,35 @@ void ghost_flood(){
 	ghosts[2].cp_y = 26;  //getPinky();
 	ghosts[3].cp_x = 1;
 	ghosts[3].cp_y = 1; //getClyde();
+    uint8_t visited[868];
 	clear_ghost_danger();
-	printf("%s\n", "Cleared");
-	for(int i=0; i<4; i++){
-		flood_node * head_fringe = (flood_node*) malloc(sizeof(flood_node));
-		//flood_node * head_closed;
-		flood_node* current_node = (flood_node*) malloc(sizeof(flood_node));
-		printf("%d\n", 28*ghosts[i].cp_x + ghosts[i].cp_y);
-		grid[ghosts[i].cp_x + 28*ghosts[i].cp_y].ghost_danger = 0;
-		printf("Assigned ghost position 0\n");
-		current_node->current_cell = grid[28*ghosts[i].cp_x + ghosts[i].cp_y];
-		//printf("%c\n", (*new_cell).food_opt);
-		//current_node->current_cell = *new_cell;
-		printf("%s\n", "About to PUsh");
-		push_flood_node(current_node, head_fringe);
-		printf("%s\n", "Called flood_node");
-		printf("%d, %d \n", head_fringe->current_cell.coordinates.cp_x,head_fringe->current_cell.coordinates.cp_y); 
-		int depth =1; 
-		while (1){
-			if(head_fringe == NULL){
+	for(int i=0; i<4; i++) {
+        memset(&visited[0], 0, sizeof(uint8_t) * 868); 
+		flood_node *head_fringe = (flood_node*) malloc(sizeof(flood_node));
+        if (head_fringe == NULL) {
+            printf("EOM. can't handle\n");
+            exit(1);
+        }
+		flood_node *tail = head_fringe;
+		head_fringe->current_cell = grid[28*ghosts[i].cp_x + ghosts[i].cp_y];
+        *(visited+28*ghosts[i].cp_x + ghosts[i].cp_y) = 1;
+		head_fringe->depth = 0;
+        head_fringe->next_flood_node = NULL;
+		while (1) {
+            printf("%d\n", i);
+            flood_node *cur_node;
+			if(head_fringe == NULL) {
 				break; 
 			}
-			free_cell cell = pop_flood_node(head_fringe);
-			flood_node* successors_head = get_legal_successors(cell, depth);
-			if(successors_head!=NULL){
-				push_flood_node(successors_head, head_fringe);
-			}
-			grid[28*cell.coordinates.cp_x + cell.coordinates.cp_y].ghost_danger = depth; 
-			depth = cell.ghost_danger +1;  
+            int empty = pop_flood_node(&head_fringe, &tail, &cur_node);
+            if (empty) {
+                printf("should not reach here!\n");
+            }
+            printf("head_fringe is %llu\n", (unsigned long long) head_fringe);
+			add_legal_successors(cur_node->depth+1, cur_node->current_cell, &visited[0], &head_fringe, &tail);
+			grid[28*cur_node->current_cell.coordinates.cp_x + cur_node->current_cell.coordinates.cp_y].ghost_danger = cur_node->depth; 
+            free(cur_node);
 		}
-		
-		free(head_fringe);
-		free(current_node);
 	}
 }
 
