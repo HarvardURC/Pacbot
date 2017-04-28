08f20985fde1614a1682c9969417fc61b4cc24da
timothy.tamm <tamm@college.harvard.edu>
2017-04-28 17:34:17 -0400

    build

--- BotCode/build.sh
+++ BotCode/build.sh
@@ -1,2 +1,4 @@
-gcc movement.c vl6180_pi.o -l wiringPi;
+gcc -std=gnu11 -c -o state.o state.c -I.
+gcc -std=gnu11 -c -o network.o network.c -I.
+gcc movement.c -o movement.o vl6180_pi.o -l wiringPi;
 make;
