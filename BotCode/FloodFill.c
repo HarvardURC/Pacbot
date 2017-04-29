e1f9d8b3667076eb1dd4918caf9aefb76e714b8f
John Holland <john.hollandr@gmai.com>
2017-04-28 20:12:18 -0400

    debug

--- BotCode/FloodFill.c
+++ BotCode/FloodFill.c
@@ -38,6 +38,7 @@ void add_legal_successors(int new_depth, free_cell pos, uint8_t *visited,
 	free_cell adjacent_cell;
     int i;
 	for(i=0; i<4; i++){
+        printf("Entered for loop");
 		adjacent_cell = grid[28*pos.adj_cell[i].cp_x +pos.adj_cell[i].cp_y];  
 		if( (adjacent_cell.food_opt != 'w') && 
                 !(new_depth == 1 && opposite(g_dir, i+1)) &&
