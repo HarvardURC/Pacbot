10c02168322fff43a2cf376ca74e301917df085f
Aaron Olkin <aaronolkin@gmail.com>
2017-09-17 00:32:02 -0400

    added communication between terminal printer and game engine

--- 2017-2018/GameEngine/gameEngine.py
+++ 2017-2018/GameEngine/gameEngine.py
@@ -1,5 +1,5 @@
 from gameState import GameState
-import time, os, sys
+import time, os, sys, struct
 import asyncio # minimum Python 3.4, changed in 3.5.1
 from stateConverter import StateConverter
 from variables import game_frequency
@@ -7,17 +7,36 @@ from variables import game_frequency
 ADDRESS = os.environ.get("BIND_ADDRESS","")
 PORT = os.environ.get("BIND_PORT", 11297)
 
+# Message sizes are communicated as a single unsigned short
+# encoded using network byte-order (big-endian).
+# receiving libraries will need to account for this properly.
+# The header also includes a magic number (also a short) for verification
+MAGIC_HEADER = 11297
+SIZE_HEADER = struct.Struct("!HH")
+
 class GameEngine:
     def __init__(self, loop):
         self.game = GameState()
-        self.playing = False
         self.loop = loop
+        self.clients = []
+        
+        coro = asyncio.start_server(self._add_client, ADDRESS, PORT)
+        self.server = loop.run_until_complete(coro)
         
         self.loop.add_reader(sys.stdin, self.keypress)
         self.loop.call_soon(self.game_tick)
+
+    def run(self):
+        try:
+            self.loop.run_forever()
+        except KeyboardInterrupt:
+            self.quit()
+            
+    def _add_client(self, reader, writer):
+        self.clients.append(writer)
         
     def game_tick(self):
-        if self.playing:
+        if self.game.play:
             # update_pacbot_pos
             # This will become asynchronous
             self.game.next_step()
@@ -25,22 +44,28 @@ class GameEngine:
         self.loop.call_later(1.0/game_frequency, self.game_tick)
 
     def quit(self):
-        # self.cleanup()
         self.loop.stop()
-
-    def restart(self):
-        self.playing = False
-        self.game.restart()
+        self.server.close()
+        self.loop.run_until_complete(self.server.wait_closed())
+        self.loop.close()
 
     def _write_state(self):
         protobuf = StateConverter.convert_game_state_to_proto(self.game)
+        msg = protobuf.SerializeToString()
+        length = SIZE_HEADER.pack(MAGIC_HEADER, len(msg))
+        for i in self.clients:
+            if i.transport.is_closing():
+                self.clients.remove(i)
+            else:
+                i.write(length)
+                i.write(msg)
 
     def keypress(self):
         char = sys.stdin.read(1)
         if char == "r":
-            self.restart()
+            self.game.restart()
         elif char == "p":
-            self.playing = not self.playing
+            self.game.play = not self.game.play
         elif char == "q":
             self.quit()
         
@@ -50,6 +75,7 @@ def main():
 
     if os.name != "nt" and os.environ.get("CURSES", False):
         # Make the input reading nicer if not on windows
+        # This pretty much breaks any output, though
         import curses, atexit
         curses.initscr()
         curses.cbreak()
@@ -58,7 +84,7 @@ def main():
                                  curses.echo(),
                                  curses.endwin()))
 
-    loop.run_forever()
+    engine.run()
 
 if __name__ == "__main__":
     main()
--- 2017-2018/GameEngine/terminalPrinter.py
+++ 2017-2018/GameEngine/terminalPrinter.py
@@ -1,11 +1,13 @@
-import sys
+import sys, os
 from variables import *
 import pacmanState_pb2
-import time
+from google.protobuf import message as proto_message
+import time, struct, socket
 
-def get_game_state():
-    # TODO: Get game state through a socket
-    return pacmanState_pb2.PacmanState()
+ADDRESS = os.environ.get("SERVER_ADDRESS","127.0.0.1")
+PORT = os.environ.get("SERVER_PORT", 11297)
+MAGIC_HEADER = 11297
+SIZE_HEADER = struct.Struct("!HH")
 
 def parse_game_mode(mode):
     if mode == pacmanState_pb2.PacmanState.FRIGHTENED:
@@ -55,11 +57,67 @@ def display_game(state):
             print(el, end='')
         print()
 
+class FancySocket:
+    def __init__(self, addr=ADDRESS, port=PORT):
+        self.addr = addr
+        self.port = port
+
+    def connect(self):
+        self.s = socket.socket()
+        self.s.connect((self.addr, self.port))
+        self.s.setblocking(False)
+
+    def __read_data(self, size):
+        data = b""
+        while len(data) < size:
+            try:
+                data += self.s.recv(size - len(data))
+            except BlockingIOError:
+                time.sleep(0.05)
+        return data
+        
+    def read_msg(self):
+        """
+        Read a message from the socket and return it.
+        This function is allowed to return None if it cannot read a message.
+
+        This function is currently blocking, i.e. it will not return until
+        it has read a message or reached an error condition.
+
+        It is important to call this function often, otherwise messages may
+        overflow the socket's buffer.
+
+        Currently, the biggest downside of this implmentation is that it does
+        not detect if the connection is dropped, it just waits forever.
+        """
+        magic, length = SIZE_HEADER.unpack(self.__read_data(SIZE_HEADER.size))
+        if magic != MAGIC_HEADER:
+            # Uh oh, what happened?
+            self.s.close()
+            print("Magic number mismatched, reconnecting...",
+                  file=sys.stderr)
+            self.connect()
+            return
+        msg = pacmanState_pb2.PacmanState()
+        try:
+            msg.ParseFromString(self.__read_data(length))
+            return msg
+        except proto_message.Error:
+            return
+
+    # Whee, context managers
+    __enter__ = connect
+    def __exit__(self, *args):
+        self.s.close()
+
 def main():
-    while True:
-        state = get_game_state()
-        display_game(state)
-        time.sleep(1.0/10)
+    s = FancySocket()
+    with s:
+        while True:
+            msg = s.read_msg()
+            if msg:
+                display_game(msg)
+            time.sleep(1.0/10)
 
 if __name__ == "__main__":
     main()
