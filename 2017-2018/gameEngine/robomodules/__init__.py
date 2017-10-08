e8a361a16f32f010c828c29532d51cadedbc0e45
timothy.tamm <tamm@college.harvard.edu>
2017-10-08 01:07:59 -0400

    remove robomodules

--- 2017-2018/gameEngine/robomodules/__init__.py
+++
@@ -1,7 +0,0 @@
-import os
-from .server import Server
-from .protoModule import ProtoModule
-
-__path__.append(os.path.join(os.path.dirname(__file__), 'comm'))
-
-__all__ = ['Server', 'ProtoModule']
--- 2017-2018/gameEngine/robomodules/comm/constants.py
+++
@@ -1,5 +0,0 @@
-import struct
-
-_SUBSCRIBE = 15000
-MAGIC_HEADER = 17380
-SIZE_HEADER = struct.Struct("!HHH")
