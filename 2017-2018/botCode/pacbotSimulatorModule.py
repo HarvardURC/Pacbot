834bd1f95669d5a51479e70427df9eafcdda46d9
Vincent Viego <veviego@dhcp-140-247-183-25.fas.harvard.edu>
2017-10-18 01:35:25 -0400

    starting my module

--- 2017-2018/botCode/pacbotSimulatorModule.py
+++ 2017-2018/botCode/pacbotSimulatorModule.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python3
+
+import os
+import robomodules as rm
+from messages import *
+
+ADDRESS = os.environ.get("BIND_ADDRESS","localhost")
+PORT = os.environ.get("BIND_PORT", 11297)
+
+class pacbotSimulatorModule(rm.protoModule):
+    
