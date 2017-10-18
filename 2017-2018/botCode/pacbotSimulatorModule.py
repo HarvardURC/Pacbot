f7af338e2488c045cfe6f021e75dfdf7b155ac7b
Vincent Viego <veviego@Vincents-MacBook-Pro.local>
2017-10-18 14:05:19 -0400

    implemented tick with frequencey 2hz in order to prevent Pacbot from flying aroudn the course

--- 2017-2018/botCode/pacbotSimulatorModule.py
+++ 2017-2018/botCode/pacbotSimulatorModule.py
@@ -2,46 +2,52 @@
 
 import os
 import robomodules as rm
+import variables as var
 from messages import MsgType, message_buffers, PacmanState
 
 ADDRESS = os.environ.get("LOCAL_ADDRESS","localhost")
 PORT = os.environ.get("LOCAL_PORT", 11295)
 
+FREQUENCY = .5
+
 
 class PacbotSimulatorModule(rm.ProtoModule):
     def __init__(self, addr, port):
         self.subscriptions = [MsgType.PACMAN_COMMAND]
-        super().__init__(addr, port, message_buffers, MsgType, self.subscriptions)  # Frequency is 0 by default
-        self.current_location = (14, 7)  # I can't figure out how to import the starting location of pacbot
+        super().__init__(addr, port, message_buffers, MsgType, FREQUENCY, self.subscriptions)
+        self.current_location = var.pacbot_starting_pos
+        self.current_command = None
+
 
     def msg_received(self, msg, msg_type):
         if msg_type == MsgType.PacmanCommand:
-            curr_x = self.current_location[1]
-            curr_y = self.current_location[2]
-
-            # Update Pacman's location based on the command
-            if msg.Direction == 1:
-                self.current_location = (curr_x, curr_y + 1)
-            elif msg.Direction == 2:
-                self.current_location = (curr_x, curr_y - 1)
-            elif msg.Direction == 3:
-                self.current_location = (curr_x + 1, curr_y)
-            elif msg.Direction == 4:
-                self.current_location = (curr_x - 1, curr_y)
-
-            # Create a new PacmanState message and broadcast the new location
-            new_msg = PacmanState.AgentState()
-            new_msg.x = self.current_location[1]
-            new_msg.y = self.current_location[2]
-            self.write(new_msg, MsgType.PACMAN_LOCATION)
-
+            self.current_command = msg.Direction
 
     def tick(self):
-        return
+        curr_x = self.current_location[1]
+        curr_y = self.current_location[2]
+        # Update Pacman's location based on the command
+        if self.current_command == 1:
+            self.current_location = (curr_x, curr_y + 1)
+        elif self.current_command == 2:
+            self.current_location = (curr_x, curr_y - 1)
+        elif self.current_command == 3:
+            self.current_location = (curr_x + 1, curr_y)
+        elif self.current_command == 4:
+            self.current_location = (curr_x - 1, curr_y)
+
+        # Create a new PacmanState message and broadcast the new location
+        new_msg = PacmanState.AgentState()
+        new_msg.x = self.current_location[1]
+        new_msg.y = self.current_location[2]
+        self.current_message = new_msg
+        self.write(self.current_message.SerializeToString(), MsgType.PACMAN_LOCATION)
+
 
 def main():
     module = PacbotSimulatorModule(ADDRESS, PORT)
     module.run()
+
 
 if __name__ == "__main__":
     main()
