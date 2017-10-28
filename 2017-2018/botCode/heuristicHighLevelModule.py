dbf66042242d91eca96187b1c853b0f43dd1b61c
timothy Tamm <tamm@college.harvard.edu>
2017-10-27 20:27:28 -0400

    heuristic part 2

--- 2017-2018/botCode/heuristicHighLevelModule.py
+++ 2017-2018/botCode/heuristicHighLevelModule.py
@@ -11,16 +11,18 @@ from messages import MsgType, message_buffers, LightState, PacmanCommand
 ADDRESS = os.environ.get("LOCAL_ADDRESS","localhost")
 PORT = os.environ.get("LOCAL_PORT", 11295)
 
-FREQUENCY = 60
-PELLET_WEIGHT = 0.75
-GHOST_WEIGHT = 0.25
-GHOST_CUTOFF = 6
+FREQUENCY = 30
+PELLET_WEIGHT = 0.65
+GHOST_WEIGHT = 0.35
+GHOST_CUTOFF = 4
 
 class HeuristicHighLevelModule(rm.ProtoModule):
     def __init__(self, addr, port):
         self.subscriptions = [MsgType.LIGHT_STATE]
         super().__init__(addr, port, message_buffers, MsgType, FREQUENCY, self.subscriptions)
         self.state = None
+        self.previous_loc = None
+        self.direction = PacmanCommand.EAST
         self.grid = copy.deepcopy(grid)
 
     def _get_direction(self, p_loc, next_loc):
@@ -35,18 +37,22 @@ class HeuristicHighLevelModule(rm.ProtoModule):
             else:
                 return PacmanCommand.WEST
 
-    def _find_distance_of_closest_ghost(self, target_loc):
-        dists = []
-        for ghost in [self.state.red_ghost, self.state.pink_ghost, self.state.orange_ghost, self.state.blue_ghost]:
-            # TODO: Use A* instead of manhattan
-            dists.append(abs(ghost.x - target_loc[0]) + abs(ghost.y - target_loc[1]))
-        return min(dists)
+    def _find_paths_to_closest_ghosts(self, pac_loc):
+        ghosts = [self.state.red_ghost, self.state.pink_ghost, self.state.orange_ghost, self.state.blue_ghost]
+        state_paths = [(ghost.state, bfs(self.grid, pac_loc, (ghost.x, ghost.y), GHOST_CUTOFF)) for ghost in ghosts]
+        print(ghosts)
+        print(state_paths)
+        return [sp for sp in state_paths if sp[1] is not None]
 
     def _find_distance_of_closest_pellet(self, target_loc):
-         return len(bfs(self.grid, target_loc, self.state, [o])) - 1
+        print('getting pellet')
+        return len(bfs(self.grid, target_loc, [o])) - 1
 
     def _target_is_invalid(self, target_loc):
         return self.grid[target_loc[0]][target_loc[1]] in [I, n]
+
+    def _is_power_pellet_closer(self, path):
+        return O in path
 
     def _find_best_target(self, p_loc):
         targets = [p_loc, (p_loc[0] - 1, p_loc[1]), (p_loc[0] + 1, p_loc[1]), (p_loc[0], p_loc[1] - 1), (p_loc[0], p_loc[1] + 1)]
@@ -56,12 +62,33 @@ class HeuristicHighLevelModule(rm.ProtoModule):
                 heuristics.append(float('inf'))
                 continue
             dist_to_pellet = self._find_distance_of_closest_pellet(target_loc)
-            dist_to_ghost = self._find_distance_of_closest_ghost(target_loc)
-            ghost_heuristic = 0 if dist_to_ghost > GHOST_CUTOFF else pow((GHOST_CUTOFF - dist_to_ghost), 2) * GHOST_WEIGHT
+            paths_to_ghosts = self._find_paths_to_closest_ghosts(target_loc)
+            closest_ghost = (None, float('inf'))
+            for state, path in paths_to_ghosts:
+                dist = len(path) - 1
+                closest_ghost = (state, dist) if dist < closest_ghost[1] else closest_ghost
+                if self._is_power_pellet_closer(path):
+                    if target_loc == p_loc:
+                        return path[1]
+                    else:
+                        return path[0]
+            ghost_heuristic = 0 if closest_ghost[1] > GHOST_CUTOFF else pow((GHOST_CUTOFF - closest_ghost[1]), 2) * GHOST_WEIGHT
+            ghost_heuristic = ghost_heuristic * -1 if closest_ghost[0] == LightState.FRIGHTENED else ghost_heuristic
+            print('ghost: {}'.format(ghost_heuristic))
             pellet_heuristic = dist_to_pellet * PELLET_WEIGHT
+            print('pellet: {}'.format(pellet_heuristic))
             heuristics.append(ghost_heuristic + pellet_heuristic)
         print(heuristics)
-        return targets[min(enumerate(heuristics), key=itemgetter(1))[0]]
+        mins = []
+        min_heur = float('inf')
+        for i, heur in enumerate(heuristics):
+            if heur < min_heur:
+                min_heur = heur
+                mins = [(i, heur)]
+            elif heur == min_heur:
+                mins.append((i, heur))
+        
+
 
     def _update_game_state(self):
         p_loc = (self.state.pacman.x, self.state.pacman.y)
@@ -77,9 +104,13 @@ class HeuristicHighLevelModule(rm.ProtoModule):
         new_msg = PacmanCommand()
         new_msg.dir = PacmanCommand.STOP
         self.write(new_msg.SerializeToString(), MsgType.PACMAN_COMMAND)
- 
+
     def msg_received(self, msg, msg_type):
         if msg_type == MsgType.LIGHT_STATE:
+            if self.previous_loc != msg.pacman:
+                if self.previous_loc is not None:
+                   self.direction = self._get_direction((self.previous_loc.x, self.previous_loc.y), (msg.pacman.x, msg.pacman.y))
+                self.previous_loc = self.state.pacman
             self.state = msg
 
     def tick(self):
@@ -92,7 +123,7 @@ class HeuristicHighLevelModule(rm.ProtoModule):
                 return
         self._send_stop_command()
 
-        
+
 def main():
     module = HeuristicHighLevelModule(ADDRESS, PORT)
     module.run()
--- 2017-2018/botCode/search.py
+++ 2017-2018/botCode/search.py
@@ -1,7 +1,7 @@
 from variables import *
 import copy
 
-def bfs(grid, start, state, targets):
+def bfs(grid, start, target, max_dist=float("inf")):
     visited = []
     queue = [(start, [])]
 
@@ -11,17 +11,19 @@ def bfs(grid, start, state, targets):
         new_path = copy.deepcopy(nxt[1])
         new_path.append(nxt[0])
         loc = nxt[0]
-        if grid[loc[0]][loc[1]] in targets:
+        if type(target) is tuple:
+            if target == loc:
+                return new_path
+        elif grid[loc[0]][loc[1]] in target:
             return new_path
 
-        if grid[loc[0] + 1][loc[1]] in [o, e, O] and (loc[0] + 1, loc[1]) not in visited:
+        if grid[loc[0] + 1][loc[1]] in [o, e, O] and (loc[0] + 1, loc[1]) not in visited and len(new_path) <= max_dist:
             queue.append(((loc[0] + 1, loc[1]),new_path))
-        if grid[loc[0] - 1][loc[1]] in [o, e, O] and (loc[0] - 1, loc[1]) not in visited:
+        if grid[loc[0] - 1][loc[1]] in [o, e, O] and (loc[0] - 1, loc[1]) not in visited and len(new_path) <= max_dist:
             queue.append(((loc[0] - 1, loc[1]),new_path))
-        if grid[loc[0]][loc[1] + 1] in [o, e, O] and (loc[0], loc[1] + 1) not in visited:
+        if grid[loc[0]][loc[1] + 1] in [o, e, O] and (loc[0], loc[1] + 1) not in visited and len(new_path) <= max_dist:
             queue.append(((loc[0], loc[1] + 1),new_path))
-        if grid[loc[0]][loc[1] - 1] in [o, e, O] and (loc[0], loc[1] - 1) not in visited:
+        if grid[loc[0]][loc[1] - 1] in [o, e, O] and (loc[0], loc[1] - 1) not in visited and len(new_path) <= max_dist:
             queue.append(((loc[0], loc[1] - 1),new_path))
 
     return None
-
