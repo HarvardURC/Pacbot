072d19e4404408b24c342ee0915e88ca376b141c
Jambay Kinley <jambaykinley@Jambay.local>
2017-12-03 00:00:11 -0500

    grid and legends added

---
+++ 2017-2018/botCode/low_level/legends.py
@@ -0,0 +1,50 @@
+# Wall
+I = 1
+
+# Unreachable
+e = 2
+
+# untouchable
+n = 3
+
+""" Corner
+I,I,I
+o,c,I
+I,o,I
+"""
+c = 4
+
+""" Straight tunnel
+I,I,I
+o,s,o
+I,I,I
+"""
+s = 5
+
+""" tri-junction A
+I,I,I
+j,o,o
+I,o,I
+"""
+j = 6
+
+""" tri-junction B
+I,I,I
+o,o,o
+I,k,I
+"""
+k = 7
+
+""" tri-junction Main
+I,I,I
+o,l,o
+I,o,I
+"""
+l = 8
+
+""" Crossroads
+I,o,I
+o,r,o
+I,o,O
+"""
+r = 9
