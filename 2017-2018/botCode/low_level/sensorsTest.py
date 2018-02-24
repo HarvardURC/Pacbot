65b049f165d22376a6d4e171a44168ea4b4e84ca
Jambay <jambay606@yahoo.com>
2018-02-24 00:24:55 -0500

    s

--- 2017-2018/botCode/low_level/sensorsTest.py
+++ 2017-2018/botCode/low_level/sensorsTest.py
@@ -2,7 +2,7 @@ from pins import *
 from sensors import Sensors
 from time import sleep
 
-sensors = Sensors([pins.tof_left, pins.tof_right], ["left", "right"], [0x29, 0x30])
+sensors = Sensors([pins.tof_fleft, pins.tof_fright], ["left", "right"], [0x29, 0x30])
 while True:
     sensors.print_all_values()
     sleep(1)
