339ed3bb00456a7182017701ff4e10f759bd7e6d
JKinx <jambay606@yahoo.com>
2018-03-24 18:31:27 -0400

    test

--- 2017-2018/botCode/low_level/sensorsTest.py
+++ 2017-2018/botCode/low_level/sensorsTest.py
@@ -2,7 +2,7 @@ from pins import *
 from sensors import Sensors
 from time import sleep
 
-sensors = Sensors([pins.tof_fleft,pins.tof_front,pins.tof_fright,pins.tof_rear, pins.tof_rright, pins.tof_rleft], ["fleft", "front","fright","rear","rleft","rright"], [0x30,0x31,0x32,0x33,0x34])
+sensors = Sensors([pins.tof_fleft,pins.tof_front,pins.tof_fright,pins.tof_rear, pins.tof_rright, pins.tof_rleft], ["fleft", "front","fright","rear","rleft","rright"], [0x30,0x31,0x32,0x33,0x34,0x35])
 while True:
     sensors.print_all_values()
     sleep(1)
