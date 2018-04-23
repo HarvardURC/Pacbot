ff67ff42fa485ca9b7a29e073782e5716c1c1ec1
Jambay <jambay606@yahoo.com>
2018-04-23 19:53:47 -0400

    encoder

--- 2017-2018/botCode/low_level/rotary_encoder_c/setup.py
+++ 2017-2018/botCode/low_level/rotary_encoder_c/setup.py
@@ -1,3 +1,3 @@
-from distutils.core import setup, Extension
-setup(name = 'Encoder', version = '1.0',  \
-   ext_modules = [Extension('Encoder', ['encoder.c'])])
+from distutils.core import setup
+from distutils.extension import Extension
+setup(name = 'Encoder', version = '1.0', ext_modules = [Extension('Encoder', ['encoder.c'])])
