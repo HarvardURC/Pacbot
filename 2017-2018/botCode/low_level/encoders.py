d9ed411a500301dfc127de1c813e036d796d4b87
Jambay <jambay606@yahoo.com>
2018-04-23 01:29:21 -0400

    new encoder code

--- 2017-2018/botCode/low_level/encoders.py
+++ 2017-2018/botCode/low_level/encoders.py
@@ -1,43 +1,48 @@
 from time import sleep
 from .GPIOhelpers import *
 #from GPIOhelpers import *
+import pigpio
 setGPIO()
 
 class Encoder:
     def __init__(self, pin_a, pin_b):
+        pi = pigpio.pi()
         self._pin_a = pin_a
         self._pin_b = pin_b
         self._position = 0
 
-        digitalMode(self._pin_a, INPUT)
-        digitalMode(self._pin_b, INPUT)
+        pi.set_mode(self._pin_a, pigpio.INPUT)
+        pi.set_mode(self._pin_b, pigpio.INPUT)
+        # digitalMode(self._pin_a, INPUT)
+        # digitalMode(self._pin_b, INPUT)
 
+        cb1 = pi.callback(self._pin_a, pigpio.EITHER_EDGE, self._interrupt_a)
+        cb2 = pi.callback(self._pin_b,  pigpio.EITHER_EDGE, self._interrupt_b)
+        # attachInterrupt(self._pin_a, self._interrupt_a, CHANGE);
+        # attachInterrupt(self._pin_b, self._interrupt_b, CHANGE);
 
-        attachInterrupt(self._pin_a, self._interrupt_a, CHANGE);
-        attachInterrupt(self._pin_b, self._interrupt_b, CHANGE);
-
-    def _interrupt_a(self, channel):
-        if (digitalRead(self._pin_a) == HIGH):
-            if (digitalRead(self._pin_b) == LOW):
+    def _interrupt_a(self, gpio, level, tick):
+        if (pi.read(self._pin_a) == 1):
+            if (pi.read(self._pin_b) == 0):
                 self._position += 1
             else:
                 self._position -= 1
 
         else:
-            if (digitalRead(self._pin_b) == LOW):
+            if (pi.read(self._pin_b) == 0):
                 self._position -= 1
             else:
                 self._position += 1
 
-    def _interrupt_b(self, channel):
-        if (digitalRead(self._pin_b) == HIGH):
-            if (digitalRead(self._pin_a) == HIGH):
+    def _interrupt_b(self, gpio, level, tick):
+        if (pi.read(self._pin_b) == 1):
+            if (pi.read(self._pin_a) == 1):
                 self._position += 1
             else:
                 self._position -= 1
 
         else:
-            if (digitalRead(self._pin_a) == LOW):
+            if (pi.read(self._pin_a) == 0):
                 self._position += 1
             else:
                 self._position -= 1
