09beb88b3eb2dd0663c62de396f1657faf28a82c
Jambay <jambay606@yahoo.com>
2018-04-23 20:25:23 -0400

    library

--- 2017-2018/botCode/low_level/rotary_encoder_c/setup.py
+++ 2017-2018/botCode/low_level/rotary_encoder_c/setup.py
@@ -1,3 +1,3 @@
 from distutils.core import setup
 from distutils.extension import Extension
-setup(name = 'Encoder', version = '1.0', libraries = ['pigpio'], ext_modules = [Extension('Encoder', ['encoder.c'])])
+setup(name = 'Encoder', version = '1.0',  ext_modules = [Extension('Encoder',libraries = ['pigpio', 'stdio', 'stdlib'], ['encoder.c'])])
