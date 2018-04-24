78fced735b300cd4ee154c73f83454cad1703a78
JKinx <jambay606@yahoo.com>
2018-04-24 13:45:34 -0400

    encoder

--- 2017-2018/botCode/low_level/rotary_encoder_c/encoder.c
+++ 2017-2018/botCode/low_level/rotary_encoder_c/encoder.c
@@ -109,7 +109,7 @@ Pi_Renc_t * renc;
 void encoderCallback(int way)
 {
    pos += way;
-   printf("new val: %d\n", pos)
+   printf("new val: %d\n", pos);
 };
 
 static PyObject* encoderRead(PyObject* self, PyObject* args){
