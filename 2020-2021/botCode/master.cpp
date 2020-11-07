4cc6387887bfdb3e7c7b6501f2018a3ead9f3de5
zevbo <minsky-pz@bxscience.edu>
2020-11-07 15:11:06 -0500

    adding botcode stuff

--- 2020-2021/botCode/master.cpp
+++ 2020-2021/botCode/master.cpp
@@ -2,18 +2,28 @@
 #include <chrono>
 #include <thread>
 
-double Master::TICK_LENGTH = millis_to_secs(20);
-
-void Master::on_init() { return; }
-void Master::on_periodic() { return; }
-void Master::loop() {
-    Master::on_periodic();
+void on_init() { return; }
+void on_periodic(bool *is_running) { *is_running = false; }
+void loop() {
+    bool *is_running = (bool *)malloc(1);
+    *is_running = true;
+    std::thread on_periodic(is_running);
     std::this_thread::sleep_for(
         std::chrono::milliseconds((int)secs_to_millis(TICK_LENGTH)));
-    Master::loop();
+    // Ideally this is_running system should be changed to actually check if the
+    //    thread finishes
+    if (is_running) {
+        printf(
+            "\n WARNING: on_periodic is taking longer than the TICK_LENGTH of "
+            "%d milliseconds.\n",
+            (int)secs_to_millis(TICK_LENGTH));
+        on_periodic.join();
+        printf("Loop continuing.\n");
+    }
+    loop();
 }
 
-void Master::start() {
-    Master::on_init();
-    Master::loop();
+void start() {
+    on_init();
+    loop();
 }
--- 2020-2021/botCode/master.hpp
+++ 2020-2021/botCode/master.hpp
@@ -1,12 +1,6 @@
 // This file will run on loop and control the robot
 #include "utils/conversions.hpp"
-class Master {
-  private:
-    static void on_init();
-    static void on_periodic();
-    static void loop();
 
-  public:
-    static double TICK_LENGTH = ;
-    static void start();
-};
+const double TICK_LENGTH = millis_to_secs(20);
+
+static void start();
