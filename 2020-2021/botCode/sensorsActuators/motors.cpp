ce26051ac4135d20989fd4107198d98a556559ac
zevbo <minsky-pz@bxscience.edu>
2021-02-23 18:27:21 -0500

    adding prints

--- 2020-2021/botCode/sensorsActuators/motors.cpp
+++ 2020-2021/botCode/sensorsActuators/motors.cpp
@@ -6,6 +6,7 @@ Motors::Motors() {
     printf("Initializing Motors\n");
     this->right_motor = new Motor(pacBot_pins["motor_speed_r"],
                                   pacBot_pins["motor_direction_r"]);
+    printf("right motor iniailized\n");
     this->left_motor = new Motor(pacBot_pins["motor_speed_l"],
                                  pacBot_pins["motor_direction_l"]);
     printf("Motors Initialized Successfully\n");
--- 2020-2021/botCode/sensorsActuators/sa_interface/motor.cpp
+++ 2020-2021/botCode/sensorsActuators/sa_interface/motor.cpp
@@ -1,16 +1,19 @@
 #include "motor.hpp"
-#include <wiringPi.h>
 #include <softPwm.h>
+#include <wiringPi.h>
 
-Motor::Motor(int power_pin, int direction_pin){
+Motor::Motor(int power_pin, int direction_pin) {
     this->power_pin = power_pin;
     this->direction_pin = direction_pin;
+    printf("variables set\n");
     softPwmCreate(this->power_pin, 0, 100);
+    printf("pwm create\n");
     pinMode(this->direction_pin, OUTPUT);
+    printf("pin mode\n");
 }
 
-void Motor::move(MotorDirection direction, int speed){
-    if (direction == STOP){
+void Motor::move(MotorDirection direction, int speed) {
+    if (direction == STOP) {
         softPwmWrite(this->power_pin, 0);
     } else {
         digitalWrite(this->direction_pin, direction);
@@ -18,7 +21,4 @@ void Motor::move(MotorDirection direction, int speed){
     }
 }
 
-void Motor::stop(){
-    softPwmWrite(this->power_pin, 0);
-}
-
+void Motor::stop() { softPwmWrite(this->power_pin, 0); }
