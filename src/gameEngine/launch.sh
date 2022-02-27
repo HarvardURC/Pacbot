94ad7f135c43fa86fd8aa37996d7f4f8752b9e4d
Sam Perlman <sperlman@college.harvard.edu>
2022-02-27 12:16:57 -0500

    launch.sh now closes the tabs it creates

--- src/gameEngine/launch.sh
+++ src/gameEngine/launch.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-ttab './server.py'
-ttab './gameEngine.py'
-ttab './visualize.py -p -s -w'
+ttab "echo -n -e \"\033]0;My Window Name\007\" && ./server.py"
+ttab "echo -n -e \"\033]0;My Window Name\007\" && ./gameEngine.py"
+ttab "echo -n -e \"\033]0;My Window Name\007\" && ./visualize.py -p -s -w"
 ./keyboardInput.py
+
+osascript -e 'tell application "Terminal" to close (every window whose name contains "My Window Name")' &
